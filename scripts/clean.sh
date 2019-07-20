4d932496ad32ce3cdec024a4be006767515d233d
Wyatt Mufson <wyatt@ryucoin.com>
2019-07-20 16:18:39 -0700

    Fix circle ci

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,29 @@
-rm ONTlock.abi.json
-rm ONTlock.avm
-rm ONTlock.avm.str
-rm ONTlock.debug.json
-rm ONTlock.Func.Map
-rm ONTlock.warning
-rm -r ontology
+main="ONTlock"
+
+if [ -f "src/$main.abi.json" ]; then
+  rm src/$main.abi.json
+fi
+
+if [ -f "src/$main.avm" ]; then
+  rm src/$main.avm
+fi
+
+if [ -f "src/$main.avm.str" ]; then
+  rm src/$main.avm.str
+fi
+
+if [ -f "src/$main.debug.json" ]; then
+  rm src/$main.debug.json
+fi
+
+if [ -f "src/$main.Func.Map" ]; then
+  rm src/$main.Func.Map
+fi
+
+if [ -f "src/$main.warning" ]; then
+  rm src/$main.warning
+fi
+
+if [ -d "src/ontology" ]; then
+  rm -r src/ontology
+fi
